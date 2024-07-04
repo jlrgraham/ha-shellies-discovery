21ebc0d7f089db76f6793aee4f6645ef302c633a
Justin Graham <justin.graham@gmail.com>
2024-07-04 18:17:07 -0500

    Update to paho MQTT 2.

--- run.py
+++ run.py
@@ -72,8 +72,8 @@ with open(filename, encoding="utf8") as f:
 compiled = compile(source, filename=filename, mode="exec")
 
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code == 0:
         logger.info("MQTT: Connected to broker.")
         announce_subscribe = f"{SHELLEY_ANNOUNCE_MQTT_PREFIX}/announce"
         logger.info(f"MQTT: Subscribe: {announce_subscribe}")
@@ -83,7 +83,7 @@ def on_connect(client, userdata, flags, rc):
             logger.info(f"MQTT: Subscribe: {HA_STATUS_TOPIC}")
             client.subscribe(HA_STATUS_TOPIC)
     else:
-        logger.error(f"MQTT: Failed to connect, rc: {rc}")
+        logger.error(f"MQTT: Failed to connect, reason_code: {reason_code}")
 
 
 def on_message(client, userdata, msg):
@@ -148,7 +148,7 @@ def run():
     if MQTT_BROKER is None:
         raise Exception("MQTT_BROKER must be defined.")
 
-    client = mqtt.Client(MQTT_CLIENT_ID)
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=MQTT_CLIENT_ID)
 
     if MQTT_USERNAME is not None and MQTT_PASSWORD is not None:
         logger.info(f"MQTT: Authentication enabled, connect as: {MQTT_USERNAME}")
